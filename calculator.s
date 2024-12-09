778279741aaf817d59af766166815e432d8fb91e
Mocassleepyyhours <mocassleepyhours@gmail.com>
2024-12-08 22:12:07 -0800

    Add files via upload

--- calculator.s
+++ calculator.s
@@ -1,108 +1,229 @@
-
 //
 // X0-X2 - parameters to linux function services
 // X8 - linux function number
 //
-.include "debug.s"
 
+//update makefile "%: %.s debug.s %.c aarch64-linux-gnu-gcc $(CFLAGS) $^ -o $@" under "all: $(TARGETS)"
+
+.include "debug.s"
 .global main
 
-.extern get_input
-.extern print_machine_status
-.extern get_modulo
-.extern print_modulo
-.extern get_decimal
-.extern print_decimal
-
 .data
-// enter mmio register here
-// Number of Units (1): Allocate 1 unit of memory.
-// Unit Size (1): Each unit is 1 byte in size.
-// Fill Value (0): Initialize each byte with the value 0.
-
-calculator_state: 	.fill 1, 1, 0 // power off or on, off = 0, on = 1
-last_operation_answer: 		.fill 1, 1, 0
-
-modulo_answer: 	.fill 1 ,8, 0 // used to hold last operation data
-modulo_a: 			.word 0
-modulo_b: 			.word 0
-
-binary_store:		.space 32
-decimal_store:		.fill 1, 8, 0
-
-temp_store:			.fill 1, 4, 0
-
-.text
-// machine must have 1 8 bit MMIO register (ie. mov x0, 0x4000000000) use ldrb (load register byte) or strb (store register byte)
-// bit 0 is On/Off, 0 = off, 1 = on
-// bit 2 for lock, 0 = unlocked (user can adjust position manually), 1 = locked
-// bit 7:5 are used for 8 different intensity levels
-//
-// aarch64-linux-gnu-gcc -c calculator.s -o calculator.o
-// aarch64-linux-gnu-gcc -c input.c -o input.o
-// aarch64-linux-gnu-gcc -static calculator.o input.o -o calculator
-// qemu-aarch64 ./calculator
-
-
-main:
+	calculator_state: 	.fill 1, 1, 0 // power off or on, off = 0, on = 1
+	last_operation_answer: 		.fill 1, 1, 0
+	modulo_answer: 	.fill 1 ,8, 0 // used to hold last operation data
+	modulo_a: 			.word 0
+	modulo_b: 			.word 0
+	binary_store:		.space 32
+	decimal_store:		.fill 1, 8, 0
+	temp_store:			.fill 1, 4, 0
+	num:				.double 0.0	
+	//prev_result:		.double 0.0
+	deci_binary_store:  .fill 20, 4, 0
+	Cm_to_m_result:		.fill 1, 8, 0
+	conversion100: 		.double 100.0
+	M_to_cm_result: 	.fill 1, 8, 0
+	M_to_km_result:		.fill 1, 8, 0
+	conversion1000: 	.double 1000.0
+	km_to_m_result: 	.fill 1,8, 0
 	
+.text
+
+main: 
+		//stp lr, x5, [sp, #-16]
+		//ldp lr, x5, [sp], #16		
 calculator_off:
 	printStr "Please enter 1 to start calculator"
-	printStr "Enter -1 to exit program"
+	printStr "Enter -1 to turn off"
 	ldr x0, =temp_store
 	bl get_input
+	
 	cmp w0, #1 // checks if user enter 1 to enter calculator
-	beq calculator_on
-	//cmp w0, #-1
-	//b exit_program
+	beq main_menu
+	cmp w0, #-1
+	beq exit_program
+	
 	printStr "You didnt enter 1 to start calculator or -1 to exit program"
 	//printStr ""
 	b calculator_off
 
-calculator_on:
+main_menu:	
+	printStr "Calculator started"
+	printStr " "
+	
+calculator_on:	
+	printStr "Main Menu"
+	printStr "Enter 1 to type a basic expression"
+	printStr "Enter 2 to do basic arithmetic operations" 
+	printStr "Enter 3 for more options (go to sub-menu)"
+	printStr "Enter 4 to print out previous resuts" // it prints all arrays
+	printStr "Enter 0 to Power Off"
 	ldr x10, =calculator_state
 	strb w0, [x10]
+	
 	ldr x0, =temp_store
-	
-	printStr "Calcuulator started"
-	printStr " "
-	printStr "Main Menu"
-	printStr "Enter 1 to Enter a basic expression"
-	printStr "Enter 2 to do basic math expression tests" //IDK THIS WHERE TO DO MATH TESTS FOR USER
-	printStr "Enter 3 for more options"
-	printStr "Enter 4 to print out previous resuts" // it prints all arrays
-	printStr "Enter 0 to power off"
-	
 	bl get_input
+	
 	cmp w0, #1
 	beq calc_expression
 	
 	cmp w0, #2
-	beq math_test
+	beq basic_arithmetic
 	
 	cmp w0, #3
-	beq extra_math_expressions
+	beq sub_menu
 	
 	cmp w0, #0
-	beq calculator_off
+	beq exit_program
 	
-	b ABNORMAL_OPERATION
-	
+	printStr "Invalid input."
+	b calculator_on
 
-extra_math_expressions:
+	
+calc_expression:
+	printStr "In calc expression"
+	b calculator_on
+
+	
+basic_arithmetic:
+				//start enter 2 numbers
+first_num:
+	ldr x0, =num
+	printStr "Enter first number: "			//data type double
+	bl get_num
+	
+	ldr x1, =num
+	str d0, [x1]
+	
+	fmov d3, d0
+	
+	sub sp, sp, #16
+	str d0, [sp]
+	
+second_num:	
+	ldr x0, =num
+	printStr "Enter second number: "
+	bl get_num
+	
+	ldr x2, =num
+	str d0, [x2]
+	fmov d4, d0
+	
+	sub sp, sp, #16
+	str d0, [sp]
+	
+	ldr d4, [sp]
+	add sp, sp, #16
+	ldr d3, [sp]
+
+operations:	
+	stp d3, d4, [sp, #-16]!
+		
+	ldr x0, =temp_store
+	printStr "Enter 1 to do Addition"
+	printStr "Enter 2 to do Subtraction"
+	printStr "Enter 3 to do Multiplication"
+	printStr "Enter 4 to do Division"
+	printStr "Enter 0 to go back Main Menu"	
+	bl get_input
+	
+	ldr x2, =temp_store
+	str x0, [x2]
+	
+	ldp d3, d4, [sp], #16
+	
+	cmp x0, 0
+	beq calculator_on
+	
+	cmp x0, 1
+	beq addition
+	
+	cmp x0, 2
+	beq subtraction
+	
+	cmp x0, 3
+	beq multiplication
+	
+	cmp x0, 4
+	beq division
+	
+	printStr "Invalid input"
+	b basic_arithmetic
+	
+addition:
+	fadd d0, d3, d4	
+	stp d3, d4, [sp, #-16]! 	
+	bl print
+
+	b next
+
+subtraction:
+	fsub d0, d3, d4	
+	stp d3, d4, [sp, #-16]! 	
+	bl print
+
+	b next
+
+multiplication:
+	fmul d0, d3, d4	
+	stp d3, d4, [sp, #-16]! 	
+	bl print
+
+	b next
+
+division:
+	fcmp d4, 0
+	beq undefined
+	
+	fdiv d0, d3, d4	
+	stp d3, d4, [sp, #-16]! 	
+	bl print
+
+	b next
+	
+undefined:
+	stp d3, d4, [sp, #-16]! 
+	printStr "Undefined. Divisor must be not equal to 0"
+	b next
+	
+next:	
+	
+	ldr x0, =temp_store
+	printStr "\nEnter 1 to continue using same numbers for other operations"
+	printStr "Enter 2 to input new numbers/ back to operations menu"
+	printStr "Any number to go back to Main Menu"
+	bl get_input
+	
+	ldr x2, =temp_store
+	str x0, [x2]
+	ldp d3, d4, [sp], #16
+	
+	cmp x0, #1
+	beq operations
+	
+	cmp x0, #2
+	beq basic_arithmetic
+	
+	b calculator_on
+	
+	
+sub_menu:
 	ldr x0, =temp_store
 	
 	printStr "Sub-Menu"
 	printStr "Enter 1 to do Modulo ( % )"
-	printStr "Enter 2 to Find Square Roots"
-	printStr "Enter 3 to Convert Decimal to binary"
+	printStr "Enter 2 to find Square Roots"
+	printStr "Enter 3 to convert Decimal to binary"
 	printStr "Enter 4 to convert Binary to Decimal"
 	printStr "Enter 5 to convert Decimal to Hex"
-	printStr "Enter 6 to Convert Hex to Decimal"
+	printStr "Enter 6 to convert Hex to Decimal"
 	printStr "Enter 7 for Unit Conversion"
-	printStr "Enter 8 to return back to main menu"
+	printStr "Enter 8 to find value of Exponent numbers"
+	printStr "Enter 9 to find Factorial"
+	printStr "Enter 0 to return back to Main Menu"
 	
 	bl get_input
+	
 	cmp w0, #1
 	beq calc_mod
 	
@@ -125,18 +246,16 @@ extra_math_expressions:
 	beq convert_units
 	
 	cmp w0, #8
-	beq calculator_on
+	beq exponent
 	
-	b ABNORMAL_OPERATION
+	cmp w0, #9
+	beq factorial
 	
-calc_expression:
-	printStr "in calc expression"
 	b calculator_on
 	
-math_test:
-	printStr "In math test"
-	b calculator_on
-
+	//b ABNORMAL_OPERATION
+	
+	
 calc_mod: // mod is remainder (a mod b = a - (a/b)*b
 	printStr "enter two numbers to find its modulo (remainder)"
 	
@@ -173,25 +292,67 @@ calc_mod: // mod is remainder (a mod b = a - (a/b)*b
 	sdiv w12, w10, w11 // a / b
 	mul w12, w12, w11 // (a / b) * b
 	sub w0, w10, w12 // a - (a / b) * b
-
 	ldr x10, =modulo_answer
 	str w0, [x10]
 	bl print_modulo
 	printStr ""
-	b extra_math_expressions
+	b sub_menu
 
+	
 calc_sqrt:
-	printStr "in sqrt"
-	b extra_math_expressions
+	ldr x0, =num
+	printStr "Enter a number for a square root: "
+	bl get_num
 	
-convert_deci_binary:
+	ldr x1, =num
+	str d0, [x1]
+	fcmp d0, 0
+	blt error
+	
+	fmov d5, d0
+	fsqrt d0, d5
+	bl print
+	sub sp, sp, #16
+	str d0, [sp]
+	b sub_menu
+
+error:
+	printStr "Error: Input must be positive."
+	b calc_sqrt
+	
+convert_deci_binary: // NOT FINISHED NEED FINISHING
 	printStr "Enter a Decimal Number to Convert to Binary"
+	ldr x0, =temp_store
+	ldr x1, =deci_binary_store
+	bl get_decimal
+	// w0 holds decimal number
+	mov x2, #0 // holds index of array
+	mov w2, #0 // holds remainder
+	
+	cbz w0, store_zero
+	
+deci_binary_loop:
+	mov w10, #2
+	udiv w3, w0, w10
+	msub w2, w3, w10, w0
+	str w2, [x1, x2, lsl #2]
+	add x2, x2, #1
+	mov w0, w3
+	cbnz w0, deci_binary_loop
+	
+	
+	b sub_menu
+	
+store_zero:
+	printStr "IN ZERO"
+	mov w2, #0
+	//str w0, [x1, x2, lsl #2] // SOMETHING BROKEN HERE
+	// bl print_binary
+	b sub_menu
+	
+deci_binary_print:
 
 	
-	bl print_binary
-	b extra_math_expressions
-
-
 convert_binary_deci:
 	printStr "Enter Binary Number to Convert to Decimal"
 	ldr x0, =decimal_store
@@ -205,6 +366,7 @@ convert_binary_deci:
 	mov w2, #2 // used to multiply by 2
 	mov w6, w0 // w6 = w0, temp = input // a
 	mov w7, #10 // b
+	
 convert_loop: // a - (a / b) * b
 	sdiv w8, w6, w7 // (a / b)
 	mul w8, w8, w7 // (a / b) * b
@@ -228,27 +390,210 @@ convert_loop: // a - (a / b) * b
 	ldr w0, [x0]
 	bl print_decimal
 	
-	b extra_math_expressions
-
+	b sub_menu
+	
+	
 convert_deci_hex:
 	printStr "in convert deci to hex"
-	b extra_math_expressions
+	b sub_menu
+	
 	
 convert_hex_deci:
 	printStr "in hex to deci"
-	b extra_math_expressions
+	b sub_menu
+	
 	
 convert_units:
-	printStr "in convert units"
-	b extra_math_expressions
+	ldr x0, =temp_store
+	printStr "Unit Conversion"
+	printStr "Enter 1 for Length conversions"
+	printStr "Enter 2 for weight" // maybe if we have time, or someone else do
+	printStr "Enter 0 to back to sub menu"
+	bl get_input
+	
+	cmp w0, #1
+	beq length_conversion
+	
+	cmp w0, #2
+	beq weight_conversion
+	
+	cmp w0, #0
+	beq sub_menu
 
+length_conversion:
+	ldr x0, =temp_store
+	printStr "Enter 1 for Centimeter to Meters"
+	printStr "Enter 2 for Meters to Centimeter"
+	printStr "Enter 3 for Meters to Kilometers"
+	printStr "Enter 4 for Kilometers to Meters"
+	printStr "Enter 0 to exit to Unit Conversion"
+	bl get_input
+	
+	cmp w0, #1
+	beq Cm_to_M
+	
+	cmp w0, #2
+	beq M_to_Cm
+	
+	cmp w0, #3
+	beq M_to_Km
+	
+	cmp w0, #4
+	beq Km_to_M
+	
+Cm_to_M: //conversion100
+	printStr "Enter Centimeter to convert to Meter"
+	ldr x0, =Cm_to_m_result
+	bl get_cm_to_m
+	ldr x5, =conversion100 // #100
+	ldr d5, [x5]
+	fdiv d0, d0, d5 // holds value
+	str d0, [x0]
+	ldr x0, =Cm_to_m_result
+	ldr d0, [x0]
+	
+	bl print_cm_to_m
+	
+	b length_conversion
+	
+M_to_Cm:
+	printStr "Enter Meter to convert to Centimeter"
+	ldr x0, =M_to_cm_result
+	bl get_m_to_cm
+	ldr x5, =conversion100
+	ldr d5, [x5]
+	fmul d0, d0, d5
+	str d0, [x0]
+	ldr x0, =M_to_cm_result
+	ldr d0, [x0]
+	bl print_m_to_cm
+	
+	b length_conversion
+M_to_Km:
+	printStr "Enter Meter to convert to Kilometer"
+	ldr x0, =M_to_km_result
+	bl get_m_to_km
+	ldr x5, =conversion1000 // #1000
+	ldr d5, [x5]
+	fdiv d0, d0, d5
+	str d0, [x0]
+	ldr x0, =M_to_km_result
+	ldr d0, [x0]
+	bl print_m_to_km
+	
+	b length_conversion
+	
+Km_to_M:
+	printStr "Enter Kilometer to convert to Meter"
+	ldr x0, =km_to_m_result
+	bl get_km_to_m
+	ldr x5, =conversion1000
+	ldr d5, [x5]
+	fmul d0, d0, d5
+	str d0, [x0]
+	ldr x0, =km_to_m_result
+	ldr d0, [x0] 
+	bl print_km_to_m
+	
+	b length_conversion
+
+weight_conversion:
+
+
+
+exponent:
+	ldr x0, =num
+	printStr "Enter base: "			//data type double
+	bl get_num
+	
+	ldr x1, =num
+	str d0, [x1]
+	fmov d5, d0
+	sub sp, sp, #16
+	str d0, [sp]
+
+
+	ldr x0, =num
+	printStr "Enter exponent: "
+	bl get_num
+	
+	ldr x2, =num
+	str d0, [x2]
+	fmov d6, d0
+	
+	sub sp, sp, #16
+	str d0, [sp]
+	
+	ldr d6, [sp]
+	add sp, sp, #16
+	ldr d5, [sp]
+	
+	fmov d7, 1				//d7 stores exponential result, assign it = 1
+	fmov d8, 1
+	
+	fcmp d6, 0
+	beq special1	
+	blt negative	
+	
+	fcmp d6, d8
+	beq special2	
+	
+	fmov d9, 1			//d9 = 1 as d6 is positive
+loop:	
+	fmul d7, d7, d5 
+	fsub d6, d6, d8
+	fcmp d6, 0
+	beq expo_result1
+	
+	b loop 		
+	
+special1:			
+	fmov d0, d7
+	bl print
+	
+	b sub_menu
+	
+special2:
+	fmov d7, d5			
+	fmov d0, d7
+	bl print
+	
+	b sub_menu
+
+negative:
+	//printStr "Error: Calculator is now working with positive exponent only"
+	//b exponent
+	fmov d9, -1			//d9 = -1 as d6 is negative
+	fabs d6, d6
+	b loop
+
+expo_result1:
+	fcmp d9, d8
+	bne expo_result2
+	
+	fmov d0, d7
+	bl print
+	
+	b sub_menu
+
+expo_result2:
+	fdiv d0, d8, d7
+	bl print
+	
+	b sub_menu
+	
+factorial:
+
+	
 ABNORMAL_OPERATION:
-	printStr "cALCULATOR FAULT"
-	printStr "EXITING NOW"
+	printStr "CALCULATOR FAULT"
+	//printStr "TURNING OFF NOW"
+	b calculator_on
+ 
       
 exit_program:
-	printStr "exiting program"
-
+	printStr "Turning calculator off.."
+	printStr "Calculator is OFF"
 exit:        
 // Setup the parameters to exit the program
 // and then call Linux to do it.
--- input.c
+++ input.c
@@ -1,13 +1,6 @@
-
 #include <stdio.h>
-#include <stdint.h>
-#include <math.h>
-
-//inputs two64 bit signed floating point
-// velocity in m/s
-// angle in positive x theta
-
-// angle must be between 0 to pi/2 (i.e. 180)
+#include <stdint.h> 
+#include <stdlib.h>
 
 int8_t get_input(int8_t* input){
 	scanf("%hhd", input);
@@ -18,23 +11,6 @@ int8_t get_input(int8_t* input){
 void get_expression(char* expression){
 	printf("Enter an expression (e.g. ( 6 + ( 5 * 4 ) ) / 2)");
 }
-/*
-int8_t get_intensity(int8_t *intensity){
-	*intensity = -1;
-	while (*intensity < 0 || *intensity > 7){
-		scanf("%hhd", intensity);
-		if(*intensity < 0 || *intensity > 7){
-			printf("Interger must be in range of 0, to 7");
-			printf("\n");
-		}
-	}
-    //scanf("%hhd", intensity);
-    printf("\n");
-    return *intensity;
-}*/
-
-
-
 
 void print_machine_status(int8_t value){
 	printf("%d", value);
@@ -74,4 +50,56 @@ void print_decimal(int32_t value){
 void printstuff(int8_t debug){
 	printf("%d", debug);
 	printf("/n");
+}
+
+double get_num(double* num)
+{
+	scanf("%lf", num);
+	return *num;
+}
+
+void print(double a)
+{
+	printf("Result is: %.3lf", a);
+	return;
+}
+double get_cm_to_m(double *value){
+	scanf("%lf", value);
+	printf("\n");
+	return *value;
+}
+void print_cm_to_m(double value){
+		printf("Result is: %lf Meters", value);
+		printf("\n");
+		return;
+}
+double get_m_to_cm(double *value){
+	scanf("%lf", value);
+	printf("\n");
+	return *value;
+}
+void print_m_to_cm(double value){
+		printf("Result is: %lf Centimeter", value);
+		printf("\n");
+		return;
+}
+double get_m_to_km(double *value){
+	scanf("%lf", value);
+	printf("\n");
+	return *value;
+}
+void print_m_to_km(double value){
+		printf("Result is: %lf Kilometer", value);
+		printf("\n");
+		return;
+}
+double get_km_to_m(double *value){
+	scanf("%lf", value);
+	printf("\n");
+	return *value;
+}
+void print_km_to_m(double value){
+		printf("Result is: %lf Meter", value);
+		printf("\n");
+		return;
 }
